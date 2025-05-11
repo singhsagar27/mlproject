f890131eeab9cf84eeb96d1eb72877302b0fe4b8
Sagar Singh <sagarsingh27998@gmail.com>
2025-05-12 00:36:22 +0530

    added hyperparameters for model training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import sys
 # import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,14 +21,30 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    """
+    Evaluates multiple regression models and returns their performance scores.
+    
+    Arguments:
+        X_train (numpy.ndarray): Training feature data.
+        y_train (numpy.ndarray): Training target data.
+        X_test (numpy.ndarray): Testing feature data.
+        y_test (numpy.ndarray): Testing target data.
+        models (dict): Dictionary of models to evaluate.
+        params (dict): Dictionary of hyperparameters for the models.
+    """
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para= params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #train the model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
